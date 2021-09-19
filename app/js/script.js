0a267aa9bc8a6c432e981e54eda4791fe0d9f3c5
Florentin Dumitrache <dumitracheflorentincristian@gmail.com>
2021-09-19 12:07:35 +0300

    Implemented the hamburger menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,10 @@
-console.log("It works!");
+const hamburgerBtn = document.querySelector(".header__navbar__hamburger");
+const hamburgerNav = document.querySelector(".header__navbar__hamburger__nav");
+
+hamburgerBtn.addEventListener("click", () => {
+  if (hamburgerNav.style.display === "flex") {
+    hamburgerNav.style.display = "none";
+  } else {
+    hamburgerNav.style.display = "flex";
+  }
+});
